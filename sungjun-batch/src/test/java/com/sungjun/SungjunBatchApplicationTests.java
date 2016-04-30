7b4d83371ea5440c0f2cf8a0a083d0c04a8d0c84
KimSungJun <arduous1130@gmail.com>
2016-04-30 13:16:47 +0900

    Create new batch project with spring batch

---
+++ sungjun-batch/src/test/java/com/sungjun/SungjunBatchApplicationTests.java
@@ -0,0 +1,18 @@
+package com.sungjun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.sungjun.main.SungjunBatchApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SungjunBatchApplication.class)
+public class SungjunBatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
